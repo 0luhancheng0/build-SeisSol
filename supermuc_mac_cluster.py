23e4171d5efa0862d8b09f636d332a9bc6e95672
Cheng <lche0021@student.monash.edu>
2018-07-15 22:51:40 +1000

    update

---
+++ supermuc_mac_cluster.py
@@ -0,0 +1,13 @@
+compileMode='release'
+parallelization='hybrid'
+arch='$ARCH'
+order='$ORDER'
+generatedKernels='yes'
+netcdf='yes'
+netcdfDir='$SeisSolHome'
+hdf5='yes'
+hdf5Dir='$SeisSolHome'
+
+#additionally for puml mesh format
+metis = 'yes'
+metisDir='$SeisSolHome'
